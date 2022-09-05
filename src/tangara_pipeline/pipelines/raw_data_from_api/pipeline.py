4f22bbdfa1e580bac066a09c3e9327e92c4fef68
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2022-09-05 13:16:32 -0500

    Pipeline updated to use new Nodes

--- src/tangara_pipeline/pipelines/raw_data_from_api/pipeline.py
+++ src/tangara_pipeline/pipelines/raw_data_from_api/pipeline.py
@@ -5,11 +5,18 @@ generated using Kedro 0.18.1
 
 from kedro.pipeline import Pipeline, node, pipeline
 
+from .nodes import tangaras
 from .nodes import raw_data_sensors
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
+        node(
+            func=tangaras,
+            inputs=['params:start_datetime' ,'params:end_datetime'],
+            outputs='tangaras',
+            name='tangaras_node'
+        ),
         node(
             func=raw_data_sensors,
             inputs=['tangaras', 'params:start_datetime' ,'params:end_datetime'],
