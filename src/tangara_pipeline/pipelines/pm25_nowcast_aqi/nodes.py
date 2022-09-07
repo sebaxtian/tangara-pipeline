da88de6ca1447c193e9069661034a168228c8e94
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2022-09-07 11:54:40 -0500

    Initial NowCast AQI pipeline created

---
+++ src/tangara_pipeline/pipelines/pm25_nowcast_aqi/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'pm25_nowcast_aqi'
+generated using Kedro 0.18.1
+"""
---
+++ src/tangara_pipeline/pipelines/pm25_nowcast_aqi/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'pm25_nowcast_aqi'
+generated using Kedro 0.18.1
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
